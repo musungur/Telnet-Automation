04884bddd41740d804ce97815e3aa2b6d607b0b3
Robert Musungu <robertmusungu3@gmail.com>
2021-07-04 09:45:13 +0300

    modified correct syntax cmd on Telnet.py

--- Telnet.py
+++ Telnet.py
@@ -21,21 +21,21 @@ with open(f"{hosts}","r") as fi:
     for line in devices:
         tn.read("Enter username:")
         tn.write(f"{USERNAME}\n")
-        tn.read("Enter password:")
-        tn.write(f"{PASS1}\n")
+        tn.write("# you're in enable mode>\n")
+        tn.write("show version\n")
         tn.write("enable\n")
-        tn.read("Enter password:")
-        tn.write(f"{PASS2}\n")
+        tn.read("Enter privileged config mode password:")
+        tn.write(f"{PASS1}\n")
         tn.write("show run\n")
         tn.write("configure terminal\n")
 
         for ip in range(1,20,2):
             mask = "255.255.255.0"
             ip+=1
-            tn.write(f"int lo {ip}\n")
+            tn.write(f"int loopback {ip}\n")
             tn.write(f"description {line}\n") 
             tn.write(f"ip add 192.168.51.{ip} {mask}")
             tn.write("\nip osp 1 area 0\n")
             tn.write("do wr\n")
             tn.write("end\n")
-            tn.write("logout")
+            tn.write("logout\n")
